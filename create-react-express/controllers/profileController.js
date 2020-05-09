f04504cedb7aa95fe38e4b2ff832064a0f5d7c97
setocourtney <courtneyjseto@gmail.com>
2020-05-09 13:18:00 -0700

    formatting

--- create-react-express/controllers/profileController.js
+++ create-react-express/controllers/profileController.js
@@ -10,21 +10,21 @@ module.exports = {
     },
     findOne: function(req, res) {
         db.Profile.findById(req.params.id)
-        .populate("owned")
-        .populate("rented")
-        .then(data => res.json(data))
-        .catch(err => res.status(422).json(err));
+            .populate("owned")
+            .populate("rented")
+            .then(data => res.json(data))
+            .catch(err => res.status(422).json(err));
             
     },
     addOne: function(req, res) {
         const newProfile = new db.Profile(req.body);
         db.User.findOne({username: req.body.username})
-        .then(user => {
-            newProfile.userId = user._id;
-            newProfile.save()
-            .then(data => res.json(data))
-            .catch(err => res.status(422).json(err));
-        })
+            .then(user => {
+                newProfile.userId = user._id;
+                newProfile.save()
+                .then(data => res.json(data))
+                .catch(err => res.status(422).json(err));
+            })
     },
     update: function(req, res) {
         db.Profile.updateOne({ _id: req.params.id}, {$set: req.body})
