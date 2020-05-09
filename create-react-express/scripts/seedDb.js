29fa40bd15c7b1e680dda81d7e045be60485b86e
crispysodium <christinena.316@gmail.com>
2020-05-09 13:48:36 -0700

    added seed files

--- create-react-express/scripts/seedDb.js
+++ create-react-express/scripts/seedDb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = require('../models')
+const db = require('../models/')
 
 mongoose.connect(
     process.env.MONGODB_URI ||
@@ -8,7 +8,6 @@ mongoose.connect(
 
 const itemSeed = [
     {
-        _id: "1",
         owner_id: "1",
         name: "Wheelbarrow",
         description: "10 gallon",
@@ -19,7 +18,6 @@ const itemSeed = [
         created_at: "01/01/2020"
     },
     {
-        _id: "2",
         owner_id: "1",
         name: "Shovel",
         description: "Flat edge",
@@ -30,7 +28,6 @@ const itemSeed = [
         created_at: "01/01/2020"
     },
     {
-        _id: "3",
         owner_id: "2",
         name: "Electric Weed Whacker",
         description: "Black and Decker. Two batteries. Each battery should last about 20-30 minutes",
@@ -41,7 +38,6 @@ const itemSeed = [
         created_at: "01/01/2020"
     },
     {
-        _id: "4",
         owner_id: "2",
         name: "Lawn Mower",
         description: "Honda. Self-propelled",
@@ -52,7 +48,6 @@ const itemSeed = [
         created_at: "01/01/2020"
     },
     {
-        _id: "5",
         owner_id: "3",
         name: "Hammer",
         description: "Red hammer",
@@ -63,7 +58,6 @@ const itemSeed = [
         created_at: "01/01/2020"
     },
     {
-        _id: "6",
         owner_id: "3", 
         name: "Circular Saw",
         description: "Stanley. 7 inch blade.",
@@ -74,7 +68,6 @@ const itemSeed = [
         created_at: "01/01/2020"
     },
     {
-        _id: "7",
         owner_id: "4", 
         name: "Pressure Washer",
         description: "Honda. 50ft hose.",
@@ -85,7 +78,6 @@ const itemSeed = [
         created_at: "01/01/2020"
     },
     {
-        _id: "8",
         owner_id: "4",
         name: "Car Jack",
         description: "Hand pump.",
@@ -96,7 +88,6 @@ const itemSeed = [
         created_at: "01/01/2020"
     },
     {
-        _id: "9",
         owner_id: "5",
         name: "Paint Sprayer",
         description: "Interior and exterior paints.",
@@ -107,7 +98,6 @@ const itemSeed = [
         created_at: "01/01/2020"
     },
     {
-        _id: "10",
         owner_id: 5,
         name: "Chainsaw",
         description: "14 inch",
@@ -121,120 +111,93 @@ const itemSeed = [
 
 const userSeed = [
     {
-        username: "setoc",
-        password: "test"
-    },
-    {
-        username: "crispy",
-        password: "test"
-    },
-    {
-        username: "ryntm",
-        password: "test"
-    },
-    {
-        username: "brigmue",
-        password: "test"
-    },
-    {
-        username: "mitch",
-        password: "test"
-    }
-]
-
-const profileSeed = [
-    {
-        _id: "1",
         firstName: "Courtney",
         lastName: "Seto",
         password: "123",
         zipCode: 98101,
         username: "setoc",
         email: "courtney@gmail.com",
-        rented: [],
-        owned: [],
+        items: {
+            rented: [],
+            owned: ["1","2"]
+        },
         createdAt: new Date(Date.now())
     },
     {
-        _id: "2",
         firstName: "Christine",
         lastName: "Na",
         password: "123",
         zipCode: 98101,
         username: "crispy",
         email: "christine@gmail.com",
-        rented: [],
-        owned: [],
+        items: {
+            rented: [],
+            owned: ["3","4"]
+        },
         createdAt: new Date(Date.now())
     },
     {
-        _id: "3",
         firstName: "Ryan",
         lastName: "Tam",
         password: "123",
         zipCode: 98155,
         username: "ryntm",
         email: "ryan@gmail.com",
-        rented: [],
-        owned: [],
+        items: {
+            rented: [],
+            owned: ["5","6"]
+        },
         createdAt: new Date(Date.now())
     },
     {
-        _id: "4",
         firstName: "Brigham",
         lastName: "Mueller",
         password: "123",
         zipCode: 98155,
         username: "brigmue",
         email: "brigham@gmail.com",
-        rented: [],
-        owned: [],
+        items: {
+            rented: [],
+            owned: ["7","8"]
+        },
         createdAt: new Date(Date.now())
     },
     {
-        _id: "5",
         firstName: "Michele",
         lastName: "Super",
         password: "123",
         zipCode: 98101,
         username: "mitch",
         email: "michele@gmail.com",
-        rented: [],
-        owned: [],
+        items: {
+            rented: [],
+            owned: ["9","10"]
+        },
         createdAt: new Date(Date.now())
     }
 ]
 
 
-// db.Item
+db.Item
+  .remove({})
+  .then(() => db.Item.collection.insertMany(itemSeed))
+  .then(data => {
+    console.log(data.result.n + " item records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+//   db.User
 //   .remove({})
-//   .then(() => db.Item.collection.insertMany(itemSeed))
+//   .then(() => db.User.collection.insertMany(userSeed))
 //   .then(data => {
-//     console.log(data.result.n + " item records inserted!");
+//     console.log(data.result.n + " user records inserted!");
 //     process.exit(0);
 //   })
 //   .catch(err => {
 //     console.error(err);
 //     process.exit(1);
 //   });
-
-// userSeed.forEach(user => {
-//     db.User
-//     .register({username: user.username, active: false} , user.password)
-//     .then(res => { console.log(res) })
-//     .catch(err => res.status(422).json(err));
-// })
-
-
-
-db.Profile
-.remove({})
-.then(() => db.Profile.collection.insertMany(profileSeed))
-.then(data => {
-console.log(data.result.n + " user records inserted!");
-process.exit(0);
-})
-.catch(err => {
-console.error(err);
-process.exit(1);
-});
