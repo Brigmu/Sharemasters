959ae754c8077088ee1f8f83625914e59b1ca4a1
crispysodium <christinena.316@gmail.com>
2020-05-14 17:02:40 -0700

    used Container component with bulma class

--- create-react-express/client/src/pages/ListingPost/ListingPost.js
+++ create-react-express/client/src/pages/ListingPost/ListingPost.js
@@ -3,12 +3,17 @@ import './styles.css';
 import Nav from '../../components/Nav/Nav'
 // import UserForm from '../../components/UserForm/UserForm';
 import Container from '../../components/Container/Container'
+import ItemForm from '../../components/ItemForm/ItemForm';
 
 const ListingPage = (props) => {
     return (
         <div className = 'listing-page'>
             <Nav />
-            <Container class = 'listing-container'>
+            {/* used container class component from bulma */}
+            {/* <Container class = 'listing-container'>
+            </Container> */}
+            <Container>
+                <ItemForm />
             </Container>
         </div>
     )
