5860feb3ef993ec9a11a522c15c66cf92f4f5571
crispysodium <christinena.316@gmail.com>
2020-05-16 14:26:41 -0700

    added signup button for new users

---
+++ create-react-express/client/src/components/SignUpButton/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUpButton = () => {
+    return (
+        <a href="/signup" class="button is-primary is-outlined">
+            <span>Sign Up</span>
+        </a>
+    );
+}
+
+export default SignUpButton;
