5e6e2c05520df0d35bf3ceb1f1ce73b3a8409934
setocourtney <courtneyjseto@gmail.com>
2020-05-26 19:32:50 -0700

    updated profile page

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -10,7 +10,6 @@ const Signup = () => {
                 <NavLogin />
             </Nav>
             <UserForm />
-            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
         </div>
     )
 }
