f0d4f78142c90d86bf5d1c60727faffff34f13df
Brigham Mueller <brigmu@gmail.com>
2020-05-27 21:11:45 -0700

    built for deployment

---
+++ client/build/precache-manifest.a879c1667a4a36daf9bbceb909328f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7851db445159b1414f4e",
+    "url": "/static/css/main.12497618.chunk.css"
+  },
+  {
+    "revision": "7851db445159b1414f4e",
+    "url": "/static/js/main.3b1404b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2f5dcfa51452094305a4",
+    "url": "/static/css/2.88b1862b.chunk.css"
+  },
+  {
+    "revision": "2f5dcfa51452094305a4",
+    "url": "/static/js/2.5f20c9df.chunk.js"
+  },
+  {
+    "revision": "baa0088b2faa5a34d339bbc962749838",
+    "url": "/index.html"
+  }
+];
