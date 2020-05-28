1d9d268affe0706d09e9715a5bb1b1a2a91ec79d
crisypsodium <christinena.316@gmail.com>
2020-05-28 16:54:47 -0700

    fixed navbar burger

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -11,14 +11,15 @@ const Nav = (props) => {
     const [state, dispatch] = useStoreContext();
     const [toggle, setToggle] = useState({
         currentClassName: "",
-        isClicked: true
+        isClicked: true,
+        mobileColor: ""
     });
 
     let location = useLocation();
 
     const handleBurgerToggle = () => {
         if (toggle.isClicked) {
-            setToggle({isClicked: false, currentClassName: "is-active"});
+            setToggle({isClicked: false, currentClassName: "is-active", mobileColor: " is-primary is-light"});
         } else {
             setToggle({currentClassName: "", isClicked: true});
         }
@@ -36,43 +37,32 @@ const Nav = (props) => {
                 <Navbar.Item>
                     <NavLink to="/" className="inactive" activeClassName="is-active"><h1>SHAREISH</h1></NavLink>
                 </Navbar.Item> }
-                <Navbar.Burger onClick={handleBurgerToggle}/>
+                <Navbar.Burger className={toggle.currentClassName} onClick={handleBurgerToggle} />
             </Navbar.Brand>
             {/* menu collapses into burger on mobile */}
-            <Navbar.Menu className={toggle.currentClassName}>
+            <Navbar.Menu className={`${toggle.currentClassName} is-right`} color={toggle.mobileColor}>
                 <Navbar.Container position="end">
-                {location.pathname === "/profile" ?
-                    <Navbar.Item>
-                        <NavLink to="/newlisting" className="button is-primary is-light">Post a Listing</NavLink>    
-                    </Navbar.Item> : ""}
-                {location.pathname === "/profile" ?
-                    <Navbar.Item>
-                        <NavLink to="/listings" className="button is-primary is-light">Browse Listings</NavLink>    
-                    </Navbar.Item> : ""}
-                </Navbar.Container>
-                <Navbar.Container>
                 {state.user ? 
-                    <div className="navbar-item dropdown is-hoverable">
-                        <div className="dropdown-trigger">
-                            <button className="button is-primary" aria-haspopup="true" aria-controls="dropdown-menu4">
-                            { state.user ? state.user.username : ""}
-                                    <img style={ { marginLeft: "10px"} } src={state.user ? state.user.icon : ""} />
-                            <span className="icon is-small">
-                                <i className="fas fa-angle-down" aria-hidden="true"></i>
-                            </span>
-                            </button>
-                        </div>
-                        <div className="dropdown-menu" id="dropdown-menu4" role="menu">
-                            <div className="dropdown-content">
-                            <div className="dropdown-item">
-                                <NavLink activeClassName="is-active" to="/profile">Profile</NavLink> 
-                            </div>
-                            <div className="dropdown-item">
-                                <LogoutButton />
-                            </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Navbar.Item dropdown
+                    hoverable
+                    >
+                        <Navbar.Item className="user">
+                            { state.user ? <p>{state.user.username}</p> : ""}
+                            <img style={ { marginLeft: "10px"} } src={state.user ? state.user.icon : ""} />
+                        </Navbar.Item>
+                        <Navbar.Dropdown className="is-right">
+                            <NavLink className="navbar-item" to="/profile">Profile</NavLink> 
+                        {location.pathname !== "/newlisting" ?
+                            <NavLink to="/newlisting" className="navbar-item" activeClassName="is-active">Post a Listing</NavLink>    
+                            : ""}
+                        {location.pathname !== "/listings" ?
+                            <NavLink to="/listings" className="navbar-item" activeClassName="is-active">Browse Listings</NavLink>    
+                            : ""}
+                        <Navbar.Item>
+                            <LogoutButton />        
+                        </Navbar.Item>
+                        </Navbar.Dropdown>
+                    </Navbar.Item>
                 :
                 state.user || location.pathname === "/signup" ? "" :
                 <Navbar.Item> 
