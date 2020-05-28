7105d7048921cc732ba1178949df45ef839ca104
crisypsodium <christinena.316@gmail.com>
2020-05-28 16:54:14 -0700

    fixed navbar

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -25,65 +25,12 @@ function Hero(props) {
         localStorage.setItem('searchTerm', searchTermRef.current.value);
         history.push('/listings');
     }
-
-
-    const renderLogoutButton = () => {
-        if (!state.user) {
-            return (
-                <Navbar.Item dropdown hoverable href="/profile">
-                    <Navbar.Link arrowless={true} className="profile">
-                        Profile
-                    </Navbar.Link>
-                    <Navbar.Dropdown>
-                        <Navbar.Item>
-                            <LogoutButton />
-                        </Navbar.Item>
-                    </Navbar.Dropdown>
-                </Navbar.Item>
-            );
-        } 
-    }
     
     return (
-        <section className="hero is-primary is-small is-mobile">
+        <section className="hero is-primary is-small">
             {/* <!-- Hero head: will stick at the top --> */}
             <div className="hero-head"> 
                 <Nav />
-                {/* <nav className="navbar">
-                    <div className="container">
-                        <div className="navbar-brand">
-                        <NavLink to="/"className="navbar-item"> */}
-                            {/* <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo" /> */}
-                        {/* </NavLink> */}
-                        {/* need to figure out how to get navbar to show in mobile */}
-                        {/* <span className="navbar-burger burger" data-target="navbarMenuHeroA">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </span>
-                        </div>
-                        <div id="navbarMenuHeroA" className="navbar-menu">
-                            <div className="navbar-end"> */}
-                                {/* <NavLink to="/listings" className="navbar-item" activeClassName="is-active">Browse</NavLink>
-                                {/* <NavLink to="/profile" className="navbar-item" activeClassName="is-active">Profile</NavLink> */}
-                                {/* {state.user ? renderLogoutButton : <Navbar.Item><SignUpButton /></Navbar.Item>} */}
-                                {/* <NavLink to="/"className="navbar-item">
-                                    Home
-                                </NavLink>
-                                <NavLink to="/listings"className="navbar-item">
-                                    Browse
-                                </NavLink>
-                                <NavLink to="/profile"className="navbar-item" disabled>
-                                    Profile */}
-                                {/* <NavLink to="/profile" activeClassName="is-active">Profile</NavLink> */}
-                                {/* </NavLink>
-                                <span className="navbar-item">
-                                    {state.user ? <LogoutButton /> : <SignUpButton />}
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </nav> */}
             </div>
 
             {/* <!-- Hero content: will be in the middle --> */}
