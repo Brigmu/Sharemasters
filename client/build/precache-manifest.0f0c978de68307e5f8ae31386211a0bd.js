95a3bf5d0114d31c5719adbdfaf7c08b4e3d2105
crisypsodium <christinena.316@gmail.com>
2020-05-28 17:56:36 -0700

    ran build

---
+++ client/build/precache-manifest.0f0c978de68307e5f8ae31386211a0bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0c78f260e9ee782a652",
+    "url": "/static/css/main.390adcd7.chunk.css"
+  },
+  {
+    "revision": "e0c78f260e9ee782a652",
+    "url": "/static/js/main.245fab3b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9e1c1147647917f851f7",
+    "url": "/static/css/2.8fab61b1.chunk.css"
+  },
+  {
+    "revision": "9e1c1147647917f851f7",
+    "url": "/static/js/2.68470957.chunk.js"
+  },
+  {
+    "revision": "84b8559389605157c0124a9a1550e764",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a879c1667a4a36daf9bbceb909328f70.js"
+  "/precache-manifest.0f0c978de68307e5f8ae31386211a0bd.js"
 );
 
 workbox.clientsClaim();
