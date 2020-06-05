b32828934b96e2c4e0f39f926a9f540b069afcc4
crisypsodium <christinena.316@gmail.com>
2020-06-04 17:53:23 -0700

    fixed mobile breakpoint

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b7690ba836116f56214051a0a0f5bc6.js"
+  "/precache-manifest.044620f829664aa18821e7776dde7097.js"
 );
 
 workbox.clientsClaim();
